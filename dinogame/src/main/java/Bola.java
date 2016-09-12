9808ecc1801d66a824c071ddd809454d4036a352
josebaGarcia <joseba.garcia@alumni.mondragon.edu>
2016-09-12 17:51:56 +0200

    Clase Bola

--- dinogame/src/main/java/Bola.java
+++ dinogame/src/main/java/Bola.java
@@ -1,6 +1,25 @@
+import static java.lang.Math.cos;
+
 /**
  * Created by Joseba on 12/09/2016.
  */
 public class Bola {
+    int x;
+    int y;
 
+    public Bola(){
+        x=10;
+        y=10;
+    }
+
+    public boolean MovimientoBola(int x1,int parametroSalto){
+        int i=1;
+        for(i=1;i<=10;i++){
+            x = x1;
+            y += y * cos(parametroSalto) / i;
+        }
+        if(i==10) {
+            return true;
+        }else return false;
+    }
 }
