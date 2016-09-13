2738b640750149e6adcf77d54e282bffbd4a03fb
Urko Pineda Olmo <urkopineda@gmail.com>
2016-09-13 19:19:28 +0200

    Anadido el controlador del menu, se debe inicializar desde el controlador principal.

--- dinogame/src/main/java/Controller/ControladorMenu.java
+++ dinogame/src/main/java/Controller/ControladorMenu.java
@@ -1,8 +1,45 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 /**
  * Created by Gorka Olalde on 12/9/16.
  */
 public class ControladorMenu {
+
+    @FXML
+    private AnchorPane mainAnchorPane;
+    @FXML
+    private VBox vbox;
+    @FXML
+    private Button playButton;
+    @FXML
+    private Button scoresButton;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Text titleText;
+    @FXML
+    private Text copyrightText;
+
+    @FXML
+    void exitButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void scoresButtonPressed(ActionEvent event) {
+
+    }
 
 }
