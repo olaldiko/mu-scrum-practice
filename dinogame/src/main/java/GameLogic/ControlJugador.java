a15d52371d4b3589a39ac7d929c9f6cbdaf1386f
josebaGarcia <joseba.garcia@alumni.mondragon.edu>
2016-09-14 14:14:27 +0200

    Movimiento de la bola

--- dinogame/src/main/java/GameLogic/ControlJugador.java
+++ dinogame/src/main/java/GameLogic/ControlJugador.java
@@ -7,8 +7,24 @@ import Model.Jugador;
  */
 public class ControlJugador {
     Jugador jugador;
+    static float contador;
+    static float contador1;
+    static float posicionY;
 
     public void recibirInteraccion() {
 
+        if(contador<=30) {
+            posicionY = posicionY + (150 * (1 / contador));
+            contador++;
+            contador1++;
+        }else if(contador<=60) {
+            posicionY = posicionY - (150 * (1 / contador1));
+            contador1--;
+            contador++;
+        }
+        if(contador==60) {
+            contador = 0;
+            contador1 = 0;
+        }
     }
 }
