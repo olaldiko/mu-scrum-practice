dad4dbad48ac447bede06daddcead32e425aba1e
josebaGarcia <joseba.garcia@alumni.mondragon.edu>
2016-09-20 15:50:22 +0200

    Movimiento de la bola

--- dinogame/src/main/java/GameLogic/CreadorObstaculos.java
+++ dinogame/src/main/java/GameLogic/CreadorObstaculos.java
@@ -3,10 +3,19 @@ package GameLogic;
 import Model.Obstaculo;
 import javafx.collections.ObservableList;
 
+import java.util.Random;
+
+import java.util.Random;
+
+
 /**
  * Created by Gorka Olalde on 12/9/16.
  */
 public class CreadorObstaculos {
+    Random rand;
+    int cantidad;
+    int contador;
+    boolean posibleNuevo;
     private ObservableList<Obstaculo> listaObstaculos;
 
     public CreadorObstaculos(ObservableList<Obstaculo> listaObstaculos) {
@@ -18,5 +27,37 @@ public class CreadorObstaculos {
     }
     public void crearObstaculos() {
 
+    }
+    public CreadorObstaculos() {
+            cantidad = 5;
+            rand = new Random();
+            contador = 0;
+            posibleNuevo=false;
+    }
+    public boolean crearObstaculo() {
+        if(posibleNuevo) {
+            if (rand.nextBoolean() != false) {
+                if (contador < cantidad) {
+                    contador++;
+                    posibleNuevo=false;
+                    return true;
+                }
+            }
+        }
+        return false;
+        }
+    public void setObstaculo(){
+            posibleNuevo=true;
+        }
+    public boolean quitarObstaculo() {
+        if (contador < cantidad && contador != 0) {
+            contador--;
+            if (contador < 0) {
+                contador = 0;
+            }
+            return true;
+        }else{
+            return false;
+        }
     }
 }
