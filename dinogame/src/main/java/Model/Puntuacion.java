88d762dadc4e9b6e94e8566020a15e130001f56c
Gorka Olalde Mendia <gorka.olalde@gmail.com>
2016-09-28 00:53:03 +0200

    Changed ranking system to use ObservableLists instead of ArrayLists

--- dinogame/src/main/java/Model/Puntuacion.java
+++ dinogame/src/main/java/Model/Puntuacion.java
@@ -6,9 +6,45 @@ import javafx.beans.property.SimpleStringProperty;
 /**
  * Created by urko on 27/09/16.
  */
-public class Puntuacion {
+public class Puntuacion implements Comparable<Puntuacion>{
 
     public SimpleIntegerProperty puntuacion = new SimpleIntegerProperty();
+
+    public int getPuntuacion() {
+        return puntuacion.get();
+    }
+
+    public SimpleIntegerProperty puntuacionProperty() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion.set(puntuacion);
+    }
+
     public SimpleStringProperty nombre = new SimpleStringProperty();
 
+
+    public String getNombre() {
+        return nombre.get();
+    }
+
+    public SimpleStringProperty nombreProperty() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre.set(nombre);
+    }
+
+    @Override
+    public int compareTo(Puntuacion p) {
+        if (puntuacion.get() < p.getPuntuacion()) {
+            return -1;
+        } else if (puntuacion.get() == p.getPuntuacion()) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }
